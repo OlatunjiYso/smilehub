1c24aeab105f0e2769c17bbb77d1bdada116b652
OlatunjiYso <olatunji.yusuf@audaciousfutures.co>
2020-02-17 22:56:44 +0100

    create database migrations

---
+++ migrations/20200217141852_transactions.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('transactions', function (table) {
+      table.increments('transaction_id');
+      table.integer('user_id');
+      table.integer('amount')
+      table.string('type')
+      table.datetime('time_created_at').defaultTo(knex.fn.now());
+      table.foreign('user_id').references('users.user_id');
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable('transactions')
+};
